aee789271b0ca5824c869f5d0091b38347fd2875
Ryan Daniels <ryandaniels230@gmail.com>
2023-08-09 13:05:59 -0400

    added js

--- website/assets/js/custom.js
+++ website/assets/js/custom.js
@@ -1,22 +1,66 @@
-// Bootstrap Validation JS
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+//this js file will only work with content that isnt loaded by the span tag. 
+//therefore native html require is being used instead on the contact file
+
+let validForm = false;
+
+let checkEmail = () => {
+  let emailElement = document.getElementById("invalid-feedback-email");
+  let email = document.getElementById("input-email");
+
+  //check email
+  if (checkValidEmail(email.value)) {
+    emailElement.classList.add("is-valid");
+    emailElement.classList.remove("is-invalid");
+    return true;
+  }
+
+  else {
+    emailElement.classList.add("is-invalid");
+    emailElement.classList.remove("is-valid");
+    return false;
+  }
+};
+
+let checkValidEmail = (text) => {
+
+  let regName = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  if (text === null || text.length <= 0 || !regName.test(text)) {
+    return false;
+  }
+
+  return true;
+}
+
+let checkMessage = () => {
+  let messageElement = document.getElementById("invalid-feedback-message");
+  let message = document.getElementById("input-message");
+
+  //check email
+  if (message.value !== null && message.value !== undefined && message.value.length > 5)
+  {
+    messageElement.classList.add("is-valid");
+    messageElement.classList.remove("is-invalid");
+    return true;
+  }
+
+  else {
+    messageElement.classList.add("is-invalid");
+    messageElement.classList.remove("is-valid");
+    return false;
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  let form = document.getElementById("contact-form");
+  console.log(form);
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    if (checkEmail() && checkMessage()) {
+      //if all good, send the request
+      form.submit();
+    }
+  });
+});
